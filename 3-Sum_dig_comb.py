17f5e596c5498406f58edd20bc0aaa1ab4b720eb
DandyCV <dandycvc@gmail.com>
2019-10-21 15:04:26 +0300

    Datawiz

--- 3-Sum_dig_comb.py
+++ 3-Sum_dig_comb.py
@@ -11,11 +11,13 @@ def sum_digits_combination(digits_sum, digits_list):
     """Задано сума і список чисел, з яких потрібно скласти таку комбінаціяю,
     яка б дорівнювала вказаній сумі."""
     combinations = 0
+    combinations_list = []
     for L in range(1, digits_sum//min(digits_list)+1):
         for subset in itertools.combinations_with_replacement(digits_list, L):
            if sum(subset) == digits_sum:
-                print(list(subset))
+                combinations_list.append(subset)
                 combinations += 1
+    print(sorted(combinations_list))
     print(combinations)
 
 
